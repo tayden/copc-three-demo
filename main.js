51f1947667b0a9c73311e8a292098299830bfe11
Taylor Denouden <taylor.denouden@hakai.org>
2025-02-18 16:50:51 -0800

    Fix missing data source

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import COPCViewer from "./lib/copc-loader.js";
 
 const url =
-    "https://public-aco-data.s3.amazonaws.com/data/4012_PlaceGlacier/23_4012_08/23_4012_08_PlaceGlacier_LASER_WGS84_UTM10_Ellips.copc.laz";
+    "https://public-aco-data.s3.us-east-1.amazonaws.com/data/4012_PlaceGlacier/23_4012_07/23_4012_07_PlaceGlacier_LASER_WGS84_UTM10_Ellips.copc.laz";
 
 async function main() {
   console.log("Starting main function");
